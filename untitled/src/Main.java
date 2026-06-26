b341c40361f34876f9ddc6417faf7f4b2af56dae
matheusSantkk <matheusantana731@gmail.com>
2026-06-26 10:12:34 -0300

    start

---
+++ untitled/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+
+    CadastroAluno aluno1 = new CadastroAluno();
+    aluno1.nome = "Matheus";
+    aluno1.email = "teste@gmail.com";
+    aluno1.idade = 21;
+
+    aluno1.listarInformacoesAluno();
+
+  }
+}
