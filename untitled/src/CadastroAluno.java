b2a3134989df60750c6908c6d050af2b3bb522a2
matheusSantkk <matheusantana731@gmail.com>
2026-06-26 10:12:23 -0300

    start

---
+++ untitled/src/CadastroAluno.java
@@ -0,0 +1,12 @@
+public class CadastroAluno {
+
+    String nome;
+    String email;
+    int idade;
+
+    void listarInformacoesAluno() {
+        System.out.println("O nome do aluno é " + nome);
+        System.out.println("Seu e-mail é " + email);
+        System.out.println("Já a idade dele é " + idade);
+    }
+}
