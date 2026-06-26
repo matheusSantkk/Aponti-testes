a840f68538f20ccdb392801eedc7fd58e40dbc05
matheusSantkk <matheusantana731@gmail.com>
2026-06-26 11:34:46 -0300

    start

--- untitled/src/CadastroAluno.java
+++ untitled/src/CadastroAluno.java
@@ -3,10 +3,24 @@ public class CadastroAluno {
     String nome;
     String email;
     int idade;
+    double mensalidade;
+    double desconto;
+    double notaFinal;
 
-    void listarInformacoesAluno() {
-        System.out.println("O nome do aluno é " + nome);
-        System.out.println("Seu e-mail é " + email);
-        System.out.println("Já a idade dele é " + idade);
+    void saudacao() {
+        System.out.println("Seja bem-vindo ao sistema " + nome);
     }
+
+    void exibirCurso(String curso) {
+        System.out.println("O seu curso é " + curso);
+    }
+
+    double calcularValorFinal() {
+        return mensalidade - desconto;
+    }
+
+    double calcularNotas(double nota1, double nota2, double nota3) {
+        return notaFinal = (nota1 + nota2 + nota3) / 3;
+    }
+
 }
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
   public static void main(String[] args) {
 
@@ -6,8 +5,16 @@ public class Main {
     aluno1.nome = "Matheus";
     aluno1.email = "teste@gmail.com";
     aluno1.idade = 21;
+    aluno1.mensalidade = 500;
+    aluno1.desconto = 123;
 
-    aluno1.listarInformacoesAluno();
+    aluno1.saudacao();
+    aluno1.exibirCurso("Testes de Software");
+    System.out.println(aluno1.calcularValorFinal());
+
+    double media =  aluno1.calcularNotas(8,7.3,6);
+
+    System.out.printf("Media: %.1f",media);
 
   }
 }
